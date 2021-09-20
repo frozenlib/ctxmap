1342dc1c47a0f9a38e4b0a89962104a0ef03d448
frozenlib <frozenlib@users.noreply.github.com>
2021-09-20 13:33:22 +0900

    Fix example.

--- examples/example.rs
+++ examples/example.rs
@@ -1,9 +1,11 @@
 fn main() {
     ctxmap::schema!(Schema);
     ctxmap::key!(Schema { KEY_A: u32 = 10 });
+    ctxmap::key!(Schema { ref KEY_B: str = "abc" });
 
     let mut m = ctxmap::CtxMap::new();
     assert_eq!(m[&KEY_A], 10);
+    assert_eq!(&m[&KEY_B], "abc");
 
     m.with(&KEY_A, &20, |m| {
         assert_eq!(m[&KEY_A], 20);
